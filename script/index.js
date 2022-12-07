7c29b65e9f690418546b417117ad14d04537900d
igorgll <igorlimagn@gmail.com>
2022-12-07 03:06:23 -0300

    modal

--- script/index.js
+++ script/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+const modal = document.getElementById("modal");
+const closeModal = document.getElementById("close");
+const loginButton = document.getElementById("login_button");
+
+loginButton.onclick = function () {
+  modal.style.display = "block";
+  modal.classList.toggle("active");
+};
+
+closeModal.onclick = function () {
+  modal.classList.remove("active");
+  modal.style.display = "none";
+};
